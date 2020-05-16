50481e7e6991e94acdce866d01f2c8081de7fd6a
Jessica <jessicamenius@gmail.com>
2020-05-16 10:01:34 -0500

    removing unnecessary files

--- instructions.js
+++
@@ -1,7 +0,0 @@
-// var instructions = document.getElementById("instructions");
-
-// $("#start").on("click", function (event) {
-//     event.preventDefault();
-//     $("#instructions").hide();
-
-// instructions();
