2982ee1a9f8e4b55a6195aee560d5180eb34589f
Jessica <jessicamenius@gmail.com>
2020-05-16 10:00:33 -0500

    fixing display

--- questions.js
+++ questions.js
@@ -1,5 +1,24 @@
 // create an array of questions with choices and the answer
 // each group in the array has 3 keys and 3 variables
+window.timer = 100;
+var start = document.getElementById("start");
+
+start.addEventListener("click", function () {
+  displayTimer();
+  displayQuestion();
+  start.parentNode.removeChild(start);
+});
+
+function displayTimer() {
+  window.createTimer = setInterval(function () {
+    window.timer -= 1;
+    document.getElementById("timer").innerHTML =
+      "Time Remaning: " + window.timer;
+    if (window.timer === 0) {
+      endQuiz();
+    }
+  }, 1000);
+}
 
 var questions = [
   {
@@ -83,7 +102,7 @@ window.score = 0;
 function displayQuestion() {
   document.getElementById("question").innerHTML =
     questions[currentQuestion].title;
-  for (var i = 0; i < questions[currentQuestion].choices.length; i += 1) {
+  for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
     var button = document.createElement("button");
     button.type = "button";
     button.value = i;
@@ -123,4 +142,37 @@ function endQuiz() {
   clearInterval(window.createTimer);
   document.getElementById("question").innerHTML = "";
   handleEndQuiz();
+}
+
+function handleEndQuiz() {
+  var existingScores = JSON.parse(localStorage.getItem("scores"));
+  if (existingScores === null) {
+    existingScores = [];
+  }
+  existingScores.push(window.score);
+  document.getElementById("question").innerHTML = "";
+  document.getElementById("score").innerHTML = window.score;
+  document.getElementById("timer").innerHTML = "";
+  window.score = 0;
+  localStorage.setItem("scores", JSON.stringify(existingScores));
+  // list the existing scores in html
+  for (var i = 0; i < existingScores.length; i++) {
+    // display the scores on the page using
+    var listItem = document.createElement("li");
+    listItem.innerHTML = existingScores[i];
+    document.getElementById("score").append(listItem);
+  }
+  window.timer = 100;
+  currentQuestion = 0;
+  var startAgain = document.createElement("button");
+  startAgain.setAttribute("id", "start-again");
+  startAgain.innerHTML = "Start Again";
+
+  startAgain.addEventListener("click", function () {
+    displayTimer();
+    displayQuestion();
+    startAgain.parentNode.removeChild(startAgain);
+    document.getElementById("score").innerHTML = "";
+  });
+  document.querySelector("#prompt_display").appendChild(startAgain);
 }
